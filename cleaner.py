f59aa12a30f5abd877a84e888829f45bde0de32a
Hossein Hematyar <hosseinhematyar@gmail.com>
2022-10-27 15:32:50 -0700

    update

--- cleaner.py
+++ cleaner.py
@@ -3,40 +3,42 @@ from datetime import date, timedelta
 
 number_of_day = 100
 today = date.today()
-candidate_list = []
 
 
-def get_modified_date(path, days):
+def get_delta_time(days):
     delta_time = date.today() - timedelta(days=days)
+    return delta_time
+
+
+def get_modified_date(path):
     directory_date = os.path.getmtime(path)
     date_modified = date.fromtimestamp(directory_date)
-    return delta_time, date_modified
+    return date_modified
 
 
-def is_remove_candidate(directory, date_modified, delta_time):
-    if date_modified < delta_time:
-        candidate_list.append(directory)
-    return candidate_list
+def is_remove_candidate(date_modified, delta_time):
+    if date_modified > delta_time:
+        return True
+    else:
+        return False
 
 
-def remove_directory(remove_list):
-    for directory in range(len(remove_list)):
-        removed_directory = candidate_list[directory]
-        print(f'{removed_directory}Directory has been deleted')
-
-        # os.chdir(drive)
-        # shutil.rmtree('testFolder')
-        # print(file_list)
-        # print('Directory has been deleted')
+def remove_directory(directory):
+    # shutil.rmtree(directory)
+    print(f'{directory}, Directory has been deleted')
 
 
 def main(root_directory, days):
+    candidate_list = []
     for path in os.listdir(root_directory):
         directory = os.path.join(root_directory, path)
         if os.path.isdir(directory):
-            delta_time, date_modified = get_modified_date(directory, days)
-            remove_list = is_remove_candidate(directory, delta_time, date_modified)
-            remove_directory(remove_list)
+            delta_time = get_delta_time(days)
+            date_modified = get_modified_date(directory)
+            candidate_status = is_remove_candidate(date_modified, delta_time)
+            if candidate_status is True:
+                candidate_list.append(directory)
+                remove_directory(directory)
     # print(candidate_list)
 
 
