127005faac8845bf881cb363b0dba8375708c98d
Hossein Hematyar <hosseinhematyar@gmail.com>
2022-10-27 15:50:36 -0700

    update

--- cleaner.py
+++ cleaner.py
@@ -1,13 +1,8 @@
 import os
 from datetime import date, timedelta
 
-number_of_day = 100
+number_of_day = 5
 today = date.today()
-
-
-def get_delta_time(days):
-    delta_time = date.today() - timedelta(days=days)
-    return delta_time
 
 
 def get_modified_date(path):
@@ -16,16 +11,9 @@ def get_modified_date(path):
     return date_modified
 
 
-def is_remove_candidate(date_modified, delta_time):
-    if date_modified > delta_time:
-        return True
-    else:
-        return False
-
-
 def remove_directory(directory):
     # shutil.rmtree(directory)
-    print(f'{directory}, Directory has been deleted')
+    print(f'{directory},--> Directory has been deleted')
 
 
 def main(root_directory, days):
@@ -33,13 +21,13 @@ def main(root_directory, days):
     for path in os.listdir(root_directory):
         directory = os.path.join(root_directory, path)
         if os.path.isdir(directory):
-            delta_time = get_delta_time(days)
+            delta_time = date.today() - timedelta(days=days)
             date_modified = get_modified_date(directory)
-            candidate_status = is_remove_candidate(date_modified, delta_time)
-            if candidate_status is True:
+            if delta_time < date_modified:
                 candidate_list.append(directory)
-                remove_directory(directory)
-    # print(candidate_list)
+
+    for directory in candidate_list:
+        remove_directory(directory)
 
 
-main('/Users/hossein/Documents/', number_of_day)
+main('/Users/hossein/Downloads/', number_of_day)
